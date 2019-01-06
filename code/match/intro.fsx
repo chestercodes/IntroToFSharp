6157e2f0fac9c7462efc1886dadb2d93d8ce4a32
chester <chester@technicalunderstanding.co.uk>
2019-01-06 20:06:14 +0000

    added stuff

--- code/match/intro.fsx
+++ code/match/intro.fsx
@@ -4,10 +4,11 @@ let (first, second) = functionThatReturnsTuple arg
 
 // in function parameters
 let someFunction (arg1first, arg1second) arg2 =
+     // first parameter, is tuple with items matched to arg1first and arg1second  
     arg1first * arg1second * arg2 
 
 // branching using the match..with syntax
-match [something] with 
+match something with 
 | pattern1 -> expression1
 | pattern2 -> expression2
 | pattern3 -> expression3
