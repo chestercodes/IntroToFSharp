657877159dfa96e1b537a24f1ec7ee31fb91db8b
chester <chester@technicalunderstanding.co.uk>
2019-01-08 22:40:01 +0000

    added stuff

--- code/du/hotel.fsx
+++ code/du/hotel.fsx
@@ -1,11 +1,11 @@
 // define type
 type RoomName =         // type TypeName =
-    | Number of int     //     | case-identifier1 of type1
-    | Name of string    //     | case-identifier2 of type2
+    | Number of int     //     | case1 of type1
+    | Name of string    //     | case2 of type2
 
 // construct values
-let room5 = Number 5                 // let typeVal1 = case-identifier1 value1
-let grandSuite = Name "Grand Suite"  // let typeVal2 = case-identifier2 value2
+let room5 = Number 5                 // let typeVal1 = case1 value1
+let grandSuite = Name "Grand Suite"  // let typeVal2 = case2 value2
 
 let printRoomName roomName =
     match roomName with
