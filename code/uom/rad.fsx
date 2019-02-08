459876fbfa802f9b2077700aee5be1eb05766a55
chester <chester@technicalunderstanding.co.uk>
2019-02-08 22:31:40 +0000

    added stuff

--- code/uom/rad.fsx
+++ code/uom/rad.fsx
@@ -1,11 +1,15 @@
-let pi = System.Math.PI
-
 [<Measure>] type radian
 [<Measure>] type degree
-let degToRad = (2.0 * pi / 360.0) * 1.0<radian/degree> // float<radian/degree>
 
 let twoRad = 2.0<radian>              // float<radian>
 let oneHundredDeg = 100.0<degree>     // float<degree>
+
+let pi = System.Math.PI
+
+// float<radian/degree>
+let degToRad = (2.0 * pi / 360.0) * 1.0<radian/degree>
+
 let twoRadsPlus100Degrees = 
     twoRad + oneHundredDeg * degToRad // float<radian>
+
 let doesntCompile = twoRad + oneHundredDeg
--- md/units-of-measure.md
+++ md/units-of-measure.md
@@ -1,6 +1,17 @@
 
 ## Units of measure
 
+![MarsRover](assets/marsRover.jpg)
+
+---
+
+@quote[Units-of-measure are to science what types are to programming.]
+
+--- 
+
+![Radians](assets/rads.png)
+
+
 ---?code=code/uom/rad.fsx&lang=fs
 
 @[1-5](Define hotel room type)
